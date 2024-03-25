0c706eb897526845dcc1bae708932210af0fffe1
Jefferson Moreira <jeffersonmoreira.ti@gmail.com>
2024-03-25 08:16:24 -0300

    Project's First Commit

--- tipos-variaveis/src/VariablesTypes.java
+++ tipos-variaveis/src/VariablesTypes.java
@@ -1,5 +1,21 @@
-public class App {
+public class VariablesTypes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        final double VALUE_OF_PI = 3.14;
+
+        short shortNumber = 1;
+        int ordinaryNumber = shortNumber;
+        short shortNumber2 = (short) ordinaryNumber;
+
+        System.out.println("shortNumber: "+shortNumber);
+        System.out.println("ordinaryNumber: "+ordinaryNumber);
+        System.out.println("shortNumber2: "+shortNumber2);
+
+        System.out.println("VALUE_OF_PI: "+VALUE_OF_PI);
+
     }
 }
+
+
+
+//https://help.ubuntu.com/community/KVM/Installation
